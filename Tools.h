53087147f46b5746a2fa6366eb67a1115309afac
wangyudong <ext.wangyudong@jd.com>
2021-01-19 16:58:05 +0800

    提交

---
+++ Tools.h
@@ -0,0 +1,16 @@
+//
+//  Tools.h
+//  JRXFoundationTest
+//
+//  Created by super on 2021/1/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tools.m
@@ -0,0 +1,12 @@
+//
+//  Tools.m
+//  JRXFoundationTest
+//
+//  Created by super on 2021/1/19.
+//
+
+#import "Tools.h"
+
+@implementation Tools
+
+@end

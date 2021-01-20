bd4567d3385136ddc4025f11d4213ebb72f92cbb
wangyudong <ext.wangyudong@jd.com>
2021-01-20 09:37:34 +0800

    0.0.7

---
+++ testManager.h
@@ -0,0 +1,16 @@
+//
+//  testManager.h
+//  JRXFoundationTest
+//
+//  Created by super on 2021/1/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testManager.m
@@ -0,0 +1,12 @@
+//
+//  testManager.m
+//  JRXFoundationTest
+//
+//  Created by super on 2021/1/20.
+//
+
+#import "testManager.h"
+
+@implementation testManager
+
+@end
